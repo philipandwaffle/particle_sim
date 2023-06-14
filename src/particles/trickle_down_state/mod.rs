af68bd0a6fc6d27b17bf9f5aad3168414caaa1b9
philipandwaffle <philipcox197@gmail.com>
2023-06-14 20:08:33 +0100

    more skele code for state system

--- src/particles/trickle_down_state/mod.rs
+++ src/particles/trickle_down_state/mod.rs
@@ -9,14 +9,31 @@ pub mod root;
 
 #[bevy_trait_query::queryable]
 pub trait Trickles {
-    fn drip(&mut self, dreg: Dreg) {}
+    fn drip(&mut self, dreg: Dreg);
+    fn peek(&self);
 }
 
 pub enum Contents {
     Consumer(Box<dyn Designer + Send + Sync>),
 }
-impl Trickles for Contents {}
+impl Trickles for Contents {
+    fn drip(&mut self,dreg:Dreg) {
+        todo!()
+    }
+
+    fn peek(&self) {
+        todo!()
+    }
+}
 
 #[derive(Component)]
 pub struct Grid {}
-impl Trickles for Grid {}
+impl Trickles for Grid {
+    fn drip(&mut self,dreg:Dreg) {
+        todo!()
+    }
+
+    fn peek(&self) {
+        todo!()
+    }
+}
--- src/particles/trickle_down_state/root.rs
+++ src/particles/trickle_down_state/root.rs
@@ -14,7 +14,11 @@ impl Root {
     }
 }
 impl Trickles for Root {
-    fn drip(&self) {
+    fn drip(&mut self, dreg: Dreg) {
+        todo!()
+    }
+
+    fn peek(&self) {
         todo!()
     }
 }
@@ -25,45 +29,51 @@ pub struct Dreg {
     primary_interact: bool,
     secondary_interact: bool,
 }
+impl Dreg {
+    pub fn new(
+        primary_nav: Vec2,
+        secondary_nav: isize,
+        primary_interact: bool,
+        secondary_interact: bool,
+    ) -> Self {
+        return Self {
+            primary_nav,
+            secondary_nav,
+            primary_interact,
+            secondary_interact,
+        };
+    }
+}
 
 fn update_root(
-    root: ResMut<Root>,
+    mut root: ResMut<Root>,
     mut control_state: ResMut<ControlState>,
-    mut designers: Query<&mut dyn Trickles>,
+    mut vessels: Query<&mut dyn Trickles>,
 ) {
-    root.drip(control_state);
-    if root.cur_designer == -1 {
-        return;
-    }
-
-    let mut root = if let Ok(designer) = designers.get_mut(root.drip()) {
-        designer
-    } else {
-        panic!();
-    };
-
     let sen = 0.25;
-
-    root.apply_primary_nav_delta(control_state.designer_primary_nav_delta * sen);
-    root.apply_secondary_nav_delta(control_state.designer_secondary_nav_delta);
-    root.apply_primary_interact(control_state.designer_primary_interact);
-    root.apply_secondary_interact(control_state.designer_secondary_interact);
+    let dreg = Dreg::new(
+        control_state.designer_primary_nav_delta * sen,
+        control_state.designer_secondary_nav_delta,
+        control_state.designer_primary_interact,
+        control_state.designer_secondary_interact,
+    );
+    root.drip(dreg);
 
     control_state.reset_designer();
 }
 
-pub fn spawn_designers(
-    mut designer_states: ResMut<DesignerStates>,
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    while !designer_states.spawn_list.is_empty() {
-        let designer = designer_states.spawn_list.pop().unwrap();
-        let mut spawn_data =
-            designer.spawn_designer(&mut commands, &asset_server, &mut meshes, &mut materials);
-        designer_states.designers.push(spawn_data.0);
-        designer_states.designers.append(&mut spawn_data.1);
-    }
-}
+// pub fn spawn_designers(
+//     mut designer_states: ResMut<DesignerStates>,
+//     mut commands: Commands,
+//     asset_server: Res<AssetServer>,
+//     mut meshes: ResMut<Assets<Mesh>>,
+//     mut materials: ResMut<Assets<StandardMaterial>>,
+// ) {
+//     while !designer_states.spawn_list.is_empty() {
+//         let designer = designer_states.spawn_list.pop().unwrap();
+//         let mut spawn_data =
+//             designer.spawn_designer(&mut commands, &asset_server, &mut meshes, &mut materials);
+//         designer_states.designers.push(spawn_data.0);
+//         designer_states.designers.append(&mut spawn_data.1);
+//     }
+// }
